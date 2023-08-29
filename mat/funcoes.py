300db056ba26fbd6ab900eed5e92a8df3ace1581
Guilherme Sampaio Dias <gui_sampaiodias@hotmail.com>
2023-08-28 21:08:12 -0300

    + func primo

--- mat/funcoes.py
+++ mat/funcoes.py
@@ -1,3 +1,13 @@
+#
+#===============================================================================
+#
+# Autor: Guilherme Sampaio Dias
+#
+# data 28/08/2023
+#
+# Exercícios para treinar o clonar a pasta do github na pasta do pc usando o git
+#
+#================================================================================
 def media_2_valores(a,b):
     return (a+b)/2
 
@@ -8,3 +18,14 @@ def media_n_valores(a):
         total += int(x)
     media = total / len(lista)
     return f'A média é {media:.2f}'
+
+def primo(x):
+    if x < 2:
+        return f"{x} não é primo"
+    else:
+        for i in range(2, int(x/2+1) ):
+            if x % i == 0:
+                return f'{x} não é primo'
+        return f'{x} é primo'
+    
+
